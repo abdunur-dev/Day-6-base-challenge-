c61474aa635a32104c1a15a8456734432a2e4b44
Abdurhaman Nur <158743046+abdunur-dev@users.noreply.github.com>
2025-10-20 15:57:27 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+    }
+    return config
+  },
+}
+
+export default nextConfig
